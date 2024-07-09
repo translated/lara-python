0080f6131665c93285a38d3f8edafe3f987bba01
Davide Caroselli <davide.caroselli@translated.com>
2024-07-09 14:23:59 +0200

    v1.0.0

--- src/lara_sdk/__init__.py
+++ src/lara_sdk/__init__.py
@@ -3,4 +3,4 @@ from ._credentials import Credentials
 from ._errors import LaraApiError, LaraError
 from ._translator import Memory, MemoryImport, TextBlock, TextResult, Memories, Translator, TranslatePriority
 
-__version__ = "0.0.0"
+__version__ = "1.0.0"
