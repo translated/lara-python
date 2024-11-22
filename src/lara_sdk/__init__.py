542a92b7af6e39698f6ed666d9e102c9dc28b88a
Davide Caroselli <davide.caroselli@translated.com>
2024-11-22 10:51:44 +0100

    1.2.4

--- src/lara_sdk/__init__.py
+++ src/lara_sdk/__init__.py
@@ -3,4 +3,4 @@ from ._credentials import Credentials
 from ._errors import LaraApiError, LaraError
 from ._translator import Memory, MemoryImport, TextBlock, TextResult, Memories, Translator, TranslatePriority, UseCache
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
